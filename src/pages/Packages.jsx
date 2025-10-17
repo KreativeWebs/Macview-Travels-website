122efb9ad6bb6f4b44a69c466ebdf2fa4e855e87
David <ishakad041@gmail.com>
2025-10-17 13:40:23 +0100

    I started working on the packages page

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -1,32 +1,11 @@
 import React from "react";
+import HeroHeader from "./HeroHeader";
+import Hero from "./Hero";
 
 function Packages() {
   return (
     <div>
-      <div className="container-fluid bg-primary py-5 mb-5 hero-header">
-        <div className="container py-5">
-          <div className="row justify-content-center py-5">
-            <div className="col-lg-10 pt-lg-5 mt-lg-5 text-center">
-              <h1 className="display-3 text-white animated slideInDown">
-                Packages
-              </h1>
-              <nav aria-label="breadcrumb">
-                <ol className="breadcrumb justify-content-center">
-                  <li className="breadcrumb-item">
-                    <a href="/">Home</a>
-                  </li>
-                  <li
-                    className="breadcrumb-item text-white active"
-                    aria-current="page"
-                  >
-                    Packages
-                  </li>
-                </ol>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
+    <HeroHeader heroheaderbg="assets/img/2151747438.jpg" heroheadertitle="Packages" pageName="Packages" heroheaderdesc="Explore More, Spend Less. Tailored travel experiences designed to give you maximum value."/>
       {/* Package Start */}
       <div className="container-xxl py-5">
         <div className="container">
