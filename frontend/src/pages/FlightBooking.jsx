ca7bb4c1af5af665be11982d8ed7f89e65e6b6c3
David <ishakad041@gmail.com>
2025-12-22 21:22:04 +0100

    fixed how to upload a image in the flight booking page

--- frontend/src/pages/FlightBooking.jsx
+++ frontend/src/pages/FlightBooking.jsx
@@ -68,7 +68,7 @@ function FlightBooking() {
 
     try {
       const res = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/flight-bookings/upload-document`,
+        `${import.meta.env.VITE_API_BASE_URL}/api/flight-bookings/upload-document`,
         {
           method: "POST",
           body: form,
